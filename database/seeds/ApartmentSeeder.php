5f7c4c9e44b24fc60636c09b45f6880847b2de27
Enrico <78877890+erripollo@users.noreply.github.com>
2021-08-30 17:51:11 +0200

    Create faker seeder apartment

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use App\Apartment;
 
 class ApartmentSeeder extends Seeder
 {
@@ -9,8 +11,23 @@ class ApartmentSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10 ; $i++) { 
+            $apartment = new Apartment();
+            $apartment-> title = $faker->words(5, true);
+            $apartment-> city = $faker-> words(50, true);
+            $apartment-> address = $faker-> streetAddress();
+            $apartment-> latitude = $faker-> latitude($min = -90, $max = 90);
+            $apartment-> longitude = $faker-> longitude($min = -180, $max = 180);
+            $apartment-> image = $faker->imageUrl(360, 200, 'Apartments', true, $apartment->title);
+            $apartment-> description = $faker-> paragraphs(3, true);
+            $apartment-> n_rooms = $faker-> randomDigitNotNull();
+            $apartment-> n_baths = $faker-> randomDigitNotNull();
+            $apartment-> n_beds = $faker-> randomDigitNotNull();
+            $apartment-> square_maters = $faker-> numberBetween(30, 400);
+            $apartment-> visible = $faker-> boolean();
+            $apartment->save();
+        }
     }
 }

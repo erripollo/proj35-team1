4b32e3b02c87c15ccbf6abf4cf2513de4626eb21
Enrico <78877890+erripollo@users.noreply.github.com>
2021-08-30 17:59:58 +0200

    Create fillable in Apartment model

--- app/Apartment.php
+++ app/Apartment.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apartment extends Model
 {
-    //
+    protected $fillable = ['title', 'city', 'address', 'latitude', 'longitude', 'image', 'description', 'n_rooms', 'n_baths', 'n_beds', 'square_meters', 'visible'];
 }

39b5788c6d8c101d1d6b0993718326fc593be20a
aaaafr <anastasia.frassini@icloud.com>
2021-08-31 15:38:29 +0200

    rel one to many apartment - messages

--- app/Message.php
+++ app/Message.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
-    protected $fillable = ['name', 'lastname', 'email', 'body'];
+    protected $fillable = ['name', 'lastname', 'email', 'body', 'apartment_id'];
     
+    public function apartment(): BelongsTo
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }

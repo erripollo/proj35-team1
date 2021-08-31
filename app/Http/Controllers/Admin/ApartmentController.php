79fd1e6e97a94cb3739a2750cd71b635ec5a2c1e
aaaafr <anastasia.frassini@icloud.com>
2021-08-31 10:38:49 +0200

    add crud apartments

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -15,7 +15,9 @@ class ApartmentController extends Controller
      */
     public function index()
     {
-        //
+        $apartment = Apartment::all();
+
+        return view('admin.apartments.index', compact('apartments'));
     }
 
     /**
@@ -25,7 +27,7 @@ class ApartmentController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.apartments.create');
     }
 
     /**
@@ -36,7 +38,24 @@ class ApartmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'title'=>'required | max:255 | min: 4',
+            'city'=>'required | max:50 | min: 2',
+            'address'=>'required | max:255 | min: 4',
+            'latitude'=>'required',
+            'longitude'=>'required',
+            'image'=>'nullable | image | max: 150',
+            'description'=>'nullable | min: 10',
+            'n_rooms'=>'required | integer | min: 1',
+            'n_baths'=>'required | integer | min: 1',
+            'n_beds'=>'required | integer | min: 1',
+            'squared_meters'=>'nullable | integer | min:25',
+            'visible'=>'required',
+            
+        
+        ]);
+        $apartment =  Apartment::create($validate);
+        return redirect()->route('admin.apartments.index');
     }
 
     /**
@@ -47,7 +66,7 @@ class ApartmentController extends Controller
      */
     public function show(Apartment $apartment)
     {
-        //
+        return view('admin.apartments.show', compact('apartment'));
     }
 
     /**
@@ -58,7 +77,7 @@ class ApartmentController extends Controller
      */
     public function edit(Apartment $apartment)
     {
-        //
+        return view('admin.apartments.edit', compact('apartment'));
     }
 
     /**
@@ -70,7 +89,24 @@ class ApartmentController extends Controller
      */
     public function update(Request $request, Apartment $apartment)
     {
-        //
+        $validate = $request->validate([
+            'title'=>'required | max:255 | min: 4',
+            'city'=>'required | max:50 | min: 2',
+            'address'=>'required | max:255 | min: 4',
+            'latitude'=>'required',
+            'longitude'=>'required',
+            'image'=>'nullable | image | max: 150',
+            'description'=>'nullable | min: 10',
+            'n_rooms'=>'required | integer | min: 1',
+            'n_baths'=>'required | integer | min: 1',
+            'n_beds'=>'required | integer | min: 1',
+            'squared_meters'=>'nullable | integer | min:25',
+            'visible'=>'required',
+            
+        
+        ]);
+        $apartment->update($validate);
+        return redirect()->route('admin.apartments.index');
     }
 
     /**
@@ -81,6 +117,7 @@ class ApartmentController extends Controller
      */
     public function destroy(Apartment $apartment)
     {
-        //
+        $apartment->delete();
+        return redirect()->route('admin.apartaments.index');
     }
 }

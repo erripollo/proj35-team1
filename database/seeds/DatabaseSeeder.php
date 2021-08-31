415e548084805dfb506c532839c42622a8825e77
Enrico <78877890+erripollo@users.noreply.github.com>
2021-08-31 17:22:13 +0200

    update DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ApartmentSeeder::class);
         $this->call(MessageSeeder::class);
         $this->call(ServiceSeeder::class);
+        $this->call(SponsorSeeder::class);
+        $this->call(VisitSeeder::class);
     }
 }

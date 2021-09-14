205285f78e8ca61c4d91e6454bc97254ce2d5677
AndreaDex <desiderio.andrea@outlook.it>
2021-09-14 12:32:12 +0200

    fix bug address in admin create

--- app/Http/Controllers/API/ApartmentController.php
+++ app/Http/Controllers/API/ApartmentController.php
@@ -13,21 +13,34 @@ class ApartmentController extends Controller
     public function index(Request $request)
     {
         $homeCitySearch = $request->location;
-        
+
         return ApartmentResource::collection(Apartment::with(['services'])
-        //$apartmentTest = DB::table('apartments')
-        //->join('apartment_service', 'apartment_service.apartment_id' , '=', 'apartments.id')
-        //->join('services', 'apartment_service.service_id' , '=', 'services.id')
-        //->select('apartments.*','apartment_service.*')
-        ->where('visible', true )
-        //->where('address', 'like', $request->address.'%')
-        //->where('n_rooms', '>=', 1)
-        //->where('n_beds', '>=', 1)
-        //->whereIn('service_id', [1, 3])
-        ->paginate());
+            //$apartmentTest = DB::table('apartments')
+            //->join('apartment_service', 'apartment_service.apartment_id' , '=', 'apartments.id')
+            //->join('services', 'apartment_service.service_id' , '=', 'services.id')
+            //->select('apartments.*','apartment_service.*')
+            ->where('visible', true)
+            //->where('address', 'like', $request->address.'%')
+            //->where('n_rooms', '>=', 1)
+            //->where('n_beds', '>=', 1)
+            //->whereIn('service_id', [1, 3])
+            ->paginate());
         //return response()->json($apartmentTest);
 
-        return view('guest.house',compact('apartments','homeCitySearch'));
+        return view('guest.house', compact('apartments', 'homeCitySearch'));
     }
 
+    public function one(Request $request)
+    {
+        $query = $request->query();
+        $id = $query['id'];
+        return ApartmentResource::collection(Apartment::all()->where('id', '=', $id));
+
+
+        //return response()->json($apartment, 200);
+    }
+
+    public function sponsored()
+    {
+    }
 }
